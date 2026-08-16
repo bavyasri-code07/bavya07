146d183ac984502a6f56046fb79849021f78c0bd
Bavya Sri <bavyas2009@gmail.com>
2026-08-16 05:14:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -228,5 +228,16 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(
         "✨ Welcome to Bavya Sri's Portfolio!"
     );
+    const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️ Light Mode";
+    } else {
+        themeToggle.textContent = "🌙 Dark Mode";
+    }
+});
 
 });
